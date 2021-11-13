d51ab6c101c1f49516800ffcbba82af23c7162a7
beibao233 <beibao2333@gmail.com>
2021-11-13 19:35:23 +0800

    Update AIchat.py

--- saya/AIchat.py
+++ saya/AIchat.py
@@ -20,7 +20,7 @@ async def AIchat(app: GraiaMiraiApplication, group: Group, message: MessageChain
     try:
         if message.getFirst(At).target == 2835692118 and not message.asDisplay().startswith("."):
             puremsg = remPunc(message.asDisplay().replace("@2835692118 ", "", 1))
-            if talk(puremsg).find("腾讯") or talk(puremsg).find("小龙女"):
+            if "腾讯" in talk(puremsg):
                 await app.sendGroupMessage(group, MessageChain.create([
                     At(member.id), Plain(f"你好 我是{yaml_data['Basic']['BotName']}")
                 ]))
