ee2eacf67ca277b3494e39aad526f5c45232ef78
beibao233 <beibao2333@gmail.com>
2022-01-08 21:17:54 +0800

    Data Save Control

--- saya/MemberDataSaveControl/__init__.py
+++ saya/MemberDataSaveControl/__init__.py
@@ -1,4 +1,4 @@
-from saya import Including
+# from saya import Including
 
 import yaml
 import os
@@ -7,9 +7,9 @@ import atexit
 global user_data
 
 
-readme = Including(author=None, group="后端功能", functions={})
+# readme = Including(author=None, group="后端功能", functions={})
 
-# indicator stand for qq number
+path = os.path.abspath(os.path.dirname(__file__))
 
 
 class NoAliasDumper(yaml.SafeDumper):
@@ -17,33 +17,109 @@ class NoAliasDumper(yaml.SafeDumper):
         return True
 
 
-if not os.path.exists('saya/MemberDataSaveControl/data.yaml'):
+class UserRegistryError(Exception):
+    """
+        When User already registred
+        This error will raise
+    """
+    def __init__(self, m):
+        self.message = m
+
+    def __str__(self):
+        return self.message
+
+    __module__ = 'builtins'
+
+
+class UserUnregisteredError(Exception):
+    """
+        When User didn't registry
+        This error will raise
+    """
+    def __init__(self, m):
+        self.message = m
+
+    def __str__(self):
+        return self.message
+
+    __module__ = 'builtins'
+
+
+if not os.path.exists(f'{path}/data.yaml'):
     print('未检测到用户数据文件！正在创建！')
-    with open('saya/MemberDataSaveControl/data.yaml', 'w', encoding="utf-8") as f:
+    with open(f'{path}/data.yaml', 'x', encoding="utf-8") as f:
         print("创建成功！")
+    user_data = {}
 else:
-    with open('saya/MemberDataSaveControl/data.yaml', 'r', encoding="utf-8") as f:
+    with open(f'{path}/data.yaml', 'r', encoding="utf-8") as f:
         file_data = f.read()
     user_data = yaml.load(file_data, Loader=yaml.FullLoader)
 
 
 def register(indicator):
+    """
+    Registry User ID and Initialization User Data Profile
+    :param indicator: A never change indicator valve
+    :return:
+    """
     if indicator in user_data:
-        raise("UserRegistryError", "User already in data.")
+        raise UserRegistryError("User already in data.")
     else:
-        user_data[indicator]["UID"] = len(user_data) + 1
+        uid = len(user_data) + 1
+        user_data[indicator] = {"UID": uid}
 
 
-def user_save(indicator, key, data):
+def data_save(indicator, key, data):
+    """
+    Save user data in profile by key
+    :param indicator: A never change indicator valve
+    :param key: A valve to get data
+    :param data: What you wanna to store
+    :return:
+    """
     try:
         register(indicator)
-    except "UserRegistryError":
+        raise UserUnregisteredError("User isn't registry. Register First Before Use.")
+    except UserRegistryError:
         user_data[indicator][key] = data
-    raise("UserUnregisteredError", "User isn't registry. Register First Before Use.")
 
+
+def data_del(indicator, key):
+    """
+    Save user data in profile by key
+    :param indicator: A never change indicator valve
+    :param key: A valve to get data
+    :return:
+    """
+    try:
+        register(indicator)
+        raise UserUnregisteredError("User isn't registry. Register First Before Use.")
+    except UserRegistryError:
+        del user_data[indicator][key]
+
+
+def indicator_get_data(indicator, key=None):
+    """
+    Get data from indicator
+    Key is optional, If you di
+    :param indicator:
+    :param key:
+    :return:
+    """
+    if key is None:
+        return user_data[indicator]
+    else:
+        return user_data[indicator][key]
+
+
+def get_data():
+    """
+    get data
+    :return: data
+    """
+    return user_data
 
 @atexit.register
-def save_config():
-    with open('saya/MemberDataSaveControl/data.yaml', 'w', encoding="utf-8") as f:
-        print("创建成功！")
+def save_userdata():
+    with open(f'{path}/data.yaml', 'w', encoding="utf-8") as f:
         yaml.dump(user_data, f, allow_unicode=True, Dumper=NoAliasDumper)
